d04e11cc7b65e4fe079ae1f9a4ad408d7f71578a
Oliver Giordano <oliver@Olivers-MacBook-Pro.local>
2026-01-07 16:21:39 -0800

    added a random chance for the pokemon to be called ditto anyways because I thought it was funny

--- pokeLaunch.sh
+++ pokeLaunch.sh
@@ -3,6 +3,11 @@ if [[ "$TERM" == "linux" ]]
 then
     exit
 else
-    cd ~/Documents/goPokeImgQuery/
-    go run main.go responseFeilds.go && ./cmdlnImageV2 CPoke.png && cat pokeName.txt
+    cd ~/Documents/goPokeImgQuery/ || return
+    go run main.go responseFeilds.go && ./cmdlnImageV2 CPoke.png
+	 if [ "$RANDOM" -lt 3277 ]; then {
+		echo "	------------ditto-------------"	> pokeName.txt
+	 } fi
+	 cat pokeName.txt
+	 touch IRAN.txt
 fi
